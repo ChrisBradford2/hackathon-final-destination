f9581210a35fa2909a15650be443a245425fd084
Nicolas Barbarisi <nico.bar2012@gmail.com>
2024-06-24 16:53:07 +0200

    Add docker environnement.

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('config.Config')
+    app.config.from_object('instance.config.Config')
     app.config.from_pyfile('config.py', silent=True)
+
+    db.init_app(app)
 
     with app.app_context():
         from . import routes
+        db.create_all()
 
     return app
