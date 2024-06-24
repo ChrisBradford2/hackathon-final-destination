7286c1a6f7a1f282da1b2aa093b1805a4b07da72
Nicolas Barbarisi <nico.bar2012@gmail.com>
2024-06-24 18:10:11 +0200

    Add Whisper for transcription.

--- back/app/routes.py
+++ back/app/routes.py
@@ -1,5 +1,20 @@
 from flask import current_app as app
+from .transcription import transcribe_audio
+from .sentiment_analysis import analyze_sentiment
+from flask import request, jsonify
 
 @app.route('/')
 def index():
-    return "Hello World !"
+    return "Hello World!"
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    audio_file = request.files['file']
+    transcription = transcribe_audio(audio_file)
+    return jsonify({"transcription": transcription})
+
+@app.route('/analyze_sentiment', methods=['POST'])
+def sentiment_analysis():
+    text = request.json.get('text')
+    sentiment = analyze_sentiment(text)
+    return jsonify({"sentiment": sentiment})
