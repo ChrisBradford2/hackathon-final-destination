58d257aae62344f4b3c1c428867759ffe9259c4b
DrAtsiSama <dareka@outlook.fr>
2024-06-28 15:35:04 +0200

    [FEATURE][INTERFACE] upload + home

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -7,17 +7,24 @@ import { faFileAudio, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-
   const [audios, setAudios] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10; // Nombre d'éléments par page
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const [selectAll, setSelectAll] = useState(false);
 
-  // Get audios from API
+  // Fonction pour récupérer les audios depuis l'API et les trier par numeroOperation décroissant
   const fetchAudios = async () => {
     setLoading(true);
     try {
       const response = await fetch('http://localhost:5001/audios');
       const data = await response.json();
+      // Tri par numeroOperation en ordre décroissant
+      data.sort((a, b) => b.id - a.id);
       setAudios(data);
       setLoading(false);
     } catch (error) {
@@ -27,62 +34,57 @@ const Home = () => {
     }
   };
 
-  // Fetch audios on component mount
+  // Appel de fetchAudios au montage du composant
   useEffect(() => {
     fetchAudios();
   }, []);
 
-  // Pagination state
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10; // Number of items per page
-
-  // Calculate pagination values
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = audios.slice(indexOfFirstItem, indexOfLastItem);
-  const totalItems = audios.length;
-  const [selectedRows, setSelectedRows] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState(null);
-  const [selectAll, setSelectAll] = useState(false);
-
+  // Gestion de la sélection de toutes les lignes
   const handleSelectAll = () => {
     setSelectAll(!selectAll);
     if (!selectAll) {
-      setSelectedRows(currentItems.map((item) => item.numeroOperation));
+      setSelectedRows(audios.map((item) => item.numeroOperation));
     } else {
       setSelectedRows([]);
     }
   };
 
+  // Gestion de la sélection d'une ligne individuelle
   const handleSelectRow = (numeroOperation) => {
     if (selectedRows.includes(numeroOperation)) {
-      setSelectedRows(selectedRows.filter((id) => id !== numeroOperation));
+      setSelectedRows(selectedRows.filter((rowId) => rowId !== numeroOperation));
     } else {
       setSelectedRows([...selectedRows, numeroOperation]);
     }
   };
 
+  // Fonction pour ouvrir le modal
   const openModal = (content) => {
     setModalContent(content);
     setShowModal(true);
   };
 
-  // Function to handle page change
+  // Fonction pour gérer le changement de page
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
+  // Calcul des valeurs de pagination
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const totalItems = audios.length;
+  const currentItems = audios.slice(indexOfFirstItem, indexOfLastItem);
+
   return (
     <div className="home mt-20">
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg mr-8 mt-20">
-        <Link to="/hackathon-final-destination/upload" className="my-4 px-4 py-2 text-sm text-white bg-blue-600 rounded-lg hover:bg-blue-800">
+        <div className="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0  bg-blue-950 px-4 w-full">
+          {/* <DropdownTab /> */}
+          <Link to="/hackathon-final-destination/upload" className="my-4 px-4 py-2 text-sm text-white bg-blue-600 rounded-lg hover:bg-blue-800">
           <FontAwesomeIcon icon={faPlus} className="text-white" />
           <span className="ms-2">Ajouter une transcription</span>
-        </Link>
-        <div className="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-blue-950 px-4 w-full">
-          <DropdownTab />
-          <label htmlFor="table-search" className="sr-only">Search</label>
+          </Link>
+          <label htmlFor="table-search" className="sr-only">Recherche</label>
           <div className="relative">
             <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -116,21 +118,21 @@ const Home = () => {
           </thead>
           <tbody>
             {currentItems.map((item, index) => (
-              <tr key={index} className="bg-white border-bhover:bg-gray-50 odd:bg-white even:bg-gray-50 text-xs">
+              <tr key={index} className="bg-white border-b hover:bg-gray-50 odd:bg-white even:bg-gray-50 text-xs">
                 <td className="p-4">
                   <div className="flex items-center">
                     <input
                       id={`checkbox-${index}`}
                       type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500    focus:ring-2  "
-                      checked={selectedRows.includes(item.id)}
-                      onChange={() => handleSelectRow(item.id)}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+                      checked={selectedRows.includes(item.numeroOperation)}
+                      onChange={() => handleSelectRow(item.numeroOperation)}
                     />
                     <label htmlFor={`checkbox-${index}`} className="sr-only">checkbox</label>
                   </div>
                 </td>
                 <td className="p-3">
-                  <Link to={`/hackathon-final-destination/${item.id}`} className="text-blue-600 hover:text-blue-800 cursor-pointer">
+                  <Link to={`/hackathon-final-destination/${item.numeroOperation}`} className="text-blue-600 hover:text-blue-800 cursor-pointer">
                     <FontAwesomeIcon icon={faFileAudio} className="text-blue-600 hover:text-blue-800 cursor-pointer" />
                   </Link>
                 </td>
@@ -142,7 +144,7 @@ const Home = () => {
                   ) : (
                     <span className="text-red-500">Non analysé</span>
                   )}
-                  </td>
+                </td>
                 <td className="p-3">
                   {item.isInNeed ? (
                     <span className="text-green-500">Oui</span>
@@ -154,8 +156,8 @@ const Home = () => {
             ))}
           </tbody>
         </table>
-        <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 p-4" aria-label="Table navigation">
-          <span className="text-sm font-normal text-gray-500  mb-4 md:mb-0 block w-full md:inline md:w-auto">
+        <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 p-4" aria-label="Navigation de la table">
+          <span className="text-sm font-normal text-gray-500 mb-4 md:mb-0 block w-full md:inline md:w-auto">
             Voir <span className="font-semibold text-gray-900 ">
               {indexOfFirstItem + 1}-{indexOfLastItem > totalItems ? totalItems : indexOfLastItem}
             </span> de <span className="font-semibold text-gray-900 ">{totalItems} </span> éléments
@@ -165,26 +167,16 @@ const Home = () => {
               <button
                 onClick={() => paginate(currentPage - 1)}
                 disabled={currentPage === 1}
-                className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 ${currentPage === 1 ? 'cursor-not-allowed opacity-50' : ''}`}
+                className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-`}
               >
                 Précédent
               </button>
             </li>
-            {Array.from({ length: Math.ceil(totalItems / itemsPerPage) }, (_, i) => (
-              <li key={i}>
-                <button
-                  onClick={() => paginate(i + 1)}
-                  className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${currentPage === i + 1 ? 'bg-gray-100 ' : ''}`}
-                >
-                  {i + 1}
-                </button>
-              </li>
-            ))}
             <li>
               <button
                 onClick={() => paginate(currentPage + 1)}
-                disabled={currentPage === Math.ceil(totalItems / itemsPerPage)}
-                className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 ${currentPage === Math.ceil(totalItems / itemsPerPage) ? 'cursor-not-allowed opacity-50' : ''}`}
+                disabled={indexOfLastItem >= totalItems}
+                className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-`}
               >
                 Suivant
               </button>
@@ -192,7 +184,7 @@ const Home = () => {
           </ul>
         </nav>
       </div>
-      <ModalAudio showModal={showModal} setShowModal={setShowModal} content={modalContent} />
+      {showModal && <ModalAudio content={modalContent} onClose={() => setShowModal(false)} />}
     </div>
   );
 };
